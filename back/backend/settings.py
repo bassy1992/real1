1deb1142f1a4063a69845d4879b1323fd7d21798
bazy <wyarquah@gmail.com>
2026-03-28 12:48:18 -0700

    Disable CSRF middleware completely

--- back/backend/settings.py
+++ back/backend/settings.py
@@ -64,8 +64,8 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'backend.csrf_middleware.AdminCSRFExemptMiddleware',
+    # CSRF disabled - using custom login without CSRF
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
