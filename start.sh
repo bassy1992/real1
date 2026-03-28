e77a987dfb0215572f8d923c7d392c84ed86a189
bazy <wyarquah@gmail.com>
2026-03-28 04:51:33 -0700

    Add Railway build configuration files (nixpacks.toml and start.sh)

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Railway start script for Django backend
+
+cd back
+python manage.py migrate
+python manage.py collectstatic --noinput
+gunicorn backend.wsgi:application --bind 0.0.0.0:$PORT
