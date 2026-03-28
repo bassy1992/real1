790fde715a6be8e930750a52088b8ab545e2f565
bazy <wyarquah@gmail.com>
2026-03-28 16:23:19 -0700

    Remove documentation and setup files

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Railway start script for Django backend
-
-cd back
-python manage.py migrate
-python manage.py collectstatic --noinput
-gunicorn backend.wsgi:application --bind 0.0.0.0:${PORT:-8000}
