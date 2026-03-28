6cf37acfd917e69a4186e58270acddf16261c10b
bazy <wyarquah@gmail.com>
2026-03-28 12:04:45 -0700

    Create custom admin site with CSRF exemption

---
+++ back/backend/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.contrib.admin import AdminSite
+from django.views.decorators.csrf import csrf_exempt
+
+
+class CSRFExemptAdminSite(AdminSite):
+    """Custom admin site that exempts login from CSRF"""
+    
+    def login(self, request, extra_context=None):
+        # Wrap the login view with csrf_exempt
+        view = super().login
+        return csrf_exempt(view)(request, extra_context)
+
+
+# Replace the default admin site
+admin.site = CSRFExemptAdminSite()
+admin.site._registry = admin.site._registry.copy()
--- back/backend/urls.py
+++ back/backend/urls.py
@@ -18,10 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.decorators.csrf import csrf_exempt
 
-# Temporarily exempt admin login from CSRF
-admin.site.login = csrf_exempt(admin.site.login)
+# Import custom admin to apply CSRF exemption
+import backend.admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
